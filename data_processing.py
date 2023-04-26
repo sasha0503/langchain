8634433776ee91c5e0dc52cb08b71b8e9808b011
oleksandr <o.l@smoothr.de>
2023-04-26 14:58:00 +0300

    added flask api and refactor

--- data_processing.py
+++ data_processing.py
@@ -1,32 +1,48 @@
 import demjson
 
-with open("data/slack_data.txt") as f:
-    slack_data = f.read()
-slack_data = [demjson.decode(i) for i in slack_data.split(';') if i]
 
-slack_encoded = slack_data[0]
-slack_keys = [i for keys in slack_data[1:] for i in keys]
+class DataProcessor:
+    def __init__(self, data_path):
+        self.data_path = data_path
+        self.keys = None
+
+    def decode(self, id):
+        if not self.keys:
+            raise ValueError("Keys are not loaded")
+        for i in self.keys:
+            if i['id'] == id:
+                return i['name']
+
+    def process(self):
+        with open(self.data_path) as f:
+            data = f.read()
+        return data
 
 
-def decode(id):
-    for i in slack_keys:
-        if i['id'] == id:
-            return i['name']
+class SlackDataProcessor(DataProcessor):
+    def process(self):
+        with open(self.data_path) as f:
+            data = f.read()
+        data = [demjson.decode(i) for i in data.split(';') if i]
+
+        self.keys = [key for keys in data[1:] for key in keys]
+        data = data[0]
+
+        messages_in_channels = {}
+        for message in data:
+            channel = self.decode(message['channel_id'])
+            if channel not in messages_in_channels:
+                messages_in_channels[channel] = []
+            messages_in_channels[channel].append(message)
+
+        final_text = ""
+        for channel, messages in messages_in_channels.items():
+            messages_in_channels[channel] = sorted(messages, key=lambda x: x['timestamp'])
+            final_text += f"{channel} chanel\n\n"
+            for message in messages_in_channels[channel]:
+                final_text += f"{self.decode(message['user_id'])}: {message['text']}\n\n"
+        return final_text
 
 
-messages_in_channels = {}
-for message in slack_encoded:
-    channel = decode(message['channel_id'])
-    if channel not in messages_in_channels:
-        messages_in_channels[channel] = []
-    messages_in_channels[channel].append(message)
-
-final_slack_text = ""
-for channel, messages in messages_in_channels.items():
-    messages_in_channels[channel] = sorted(messages, key=lambda x: x['timestamp'])
-    final_slack_text += f"{channel} chanel\n\n"
-    for message in messages_in_channels[channel]:
-        final_slack_text += f"{decode(message['user_id'])}: {message['text']}\n\n"
-
-with open("data/slack_final_data.txt", "w") as f:
-    f.write(final_slack_text)
+class NotionDataProcessor(DataProcessor):
+    pass
